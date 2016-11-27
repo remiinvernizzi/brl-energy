273e232dc7b1b0044a5badd0f7d210d6f135a65c
Remi Invernizzi <web.invernizzi@gmail.com>
2016-11-27 09:11:28 +0100

    Preparation de la classe Espira gèrant les évenements sur la station d'Espira. Creation fichier principal index.html et mise en place de ses différentes sections

---
+++ js/espira.js
@@ -0,0 +1,31 @@
+/*******************************************************/
+/* Fichier de gestion de la station d'Espira de l'Agly */
+/*******************************************************/
+
+//Classe de la station Espira
+var Espira = {
+  //Initialisation du tableau
+  init : function(){
+    
+  },
+
+  //Affichage du tableau
+  affichage : function(){
+
+  },
+
+  //Modification d'un releve
+  modification : function(){
+
+  },
+
+  //Ajout d'un releve
+  ajout : function(){
+
+  },
+
+  //Suppression d'un releve
+  suppression : function(){
+
+  },
+};
--- js/form_espira.js
+++ js/form_espira.js
@@ -1,20 +1,36 @@
 var form = document.getElementById('form_espira');
 
+//Fonction d'ajout de relevés
+function ajoutReleve(donnees){
+
+}
+
 //Gestion de la soumission du formulaire
 form.addEventListener("submit", function(e){
   e.preventDefault();
 
+  var validForm = true;
+
   //Récupération du lien vers tous les 'input'
   var inputElts = document.getElementsByTagName("input");
-  console.log(inputElts);
   //Expression régulière d'une chaine ne contenant que des chiffres
   var regexInput = /^[0-9]+/;
 
   //Vérification de chaque valeur de 'input'
   for(var i = 0; i < inputElts.length-1; i++){
-      if(!regexInput.test(inputElts[i].value)){
-        console.log("Mauvaise Saisie");
-        inputElts[i].style.backgroundColor = "#ED9D4C";
-      };
+    if(!regexInput.test(inputElts[i].value)){
+      console.log("Mauvaise Saisie");
+      inputElts[i].style.backgroundColor = "#ED9D4C";
+      validForm = false;
+      break;
+    }
+    else{
+      inputElts[i].style.backgroundColor = "";
     };
+  };
+
+  if(validForm === true)
+  {
+    console.log("OK");
+  }
 });
