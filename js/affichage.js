efa4261ce962416b0c9ae4ff4a508a7bb111d81f
Remi Invernizzi <web.invernizzi@gmail.com>
2016-12-03 16:02:48 +0100

    Mise en forme de la classe 'EspiraForm' et ajout du bouton submit

--- js/affichage.js
+++ js/affichage.js
@@ -1,9 +1,14 @@
-//Récupération de la liste de champs à afficher
+//Récupération de la liste des champs du formulaire à afficher
 ajaxGet("http://127.0.0.1/edsa-brl-energy/json/struct_espira.json", function(reponse){
   listeInputs = JSON.parse(reponse);
+  var inputForm = Object.create(EspiraForm);
+  //Creation du squelette du nouveau formulaire
+  inputForm.creationForm();
+  //Affichage des champs
   listeInputs.forEach(function(inputElt){
-    var inputForm = Object.create(EspiraForm);
     inputForm.init(inputElt);
-    inputForm.affichage();
+    inputForm.affichageChamp();
   })
+  //Affichage du bouton de validation
+  inputForm.affichageSubmit();
 })
--- js/espiraForm.js
+++ js/espiraForm.js
@@ -12,22 +12,76 @@ var EspiraForm = {
     this.div = input.div; //Div qui contient l'input
   },
 
-  //Affichage du formulaire
-  affichage : function(){
-    //Recherche si la balise 'div' avec l'id this.div existe
-    if(!document.getElementById(this.div)){
+  //Affichage des champs du formulaire
+  affichageChamp : function(){
+    //Récupération du div s'il existe
+    var divElt = document.getElementById(this.div);
+
+    //Détection de l'identifiant du div 'energie'
+    if(this.div == "energie"){
+      //Ajout du champ au fielset
+      var fieldsetElt = this.fieldsetAdd();
+      //Ajout des champs au fieldset puis au div
+      fieldsetElt.innerHTML += "<label for=\"" + this.name + "\">" + this.label + "</label>";
+      fieldsetElt.innerHTML += "<input type=\"text\" name=\"" + this.name + "\" id=\"" + this.name + "\" size=\"10\" required/>";
+      //Mise à jour du fielset
+      divElt.appendChild(fieldsetElt);
+    }
+    else{
+      //Ajout des champs au div
+      divElt.innerHTML += "<label for=\"" + this.name + "\">" + this.label + "</label>";
+      divElt.innerHTML += "<input type=\"text\" name=\"" + this.name + "\" id=\"" + this.name + "\" size=\"10\" required/>";
+      //Mise à jour du div
+      document.getElementById("formStation").appendChild(divElt);
+    }
+  },
+
+  //Affichage du bouton de validationdu formulaire
+  affichageSubmit : function(){
+    //Creation du div du bouton de validation
+    var divSubmit = document.getElementById("submit");
+    //Ajout du bouton au div puis affichage
+    divSubmit.innerHTML += "<input type=\"submit\" value=\"Ajouter les relevés\">";
+    document.getElementById("formStation").appendChild(divSubmit);
+  },
+
+  //Creation du squellette de base du formulaire
+  creationForm : function(){
+    var divForm = document.createElement("form");
+    var divDateElt = document.createElement("div");
+    var divDonneesElt = document.createElement("div");
+    var divEnergieElt = document.createElement("div");
+    var divSubmit = document.createElement("div");
+
+    divForm.id = "formStation";
+    divDateElt.id = "date";
+    divDonneesElt.id = "donnees";
+    divEnergieElt.id = "energie";
+    divSubmit.id = "submit";
+
+    divForm.appendChild(divDateElt);
+    divForm.appendChild(divDonneesElt);
+    divForm.appendChild(divEnergieElt);
+    divForm.appendChild(divSubmit);
+
+    document.getElementById("gestion").appendChild(divForm);
+  },
+
+  //Ajout du champ dans un fieldset
+  fieldsetAdd : function(){
+    var splitted = this.name.split('_');
+    //Recherche si la balise 'fielset' avec l'id splitted[1] existe
+    if(!document.getElementById(splitted[1])){
       //Création du div s'il n'existe pas et définition de son identifiant
-      var divElt = document.createElement("div");
-      divElt.id = this.div;
+      var fieldsetElt = document.createElement("fieldset");
+      fieldsetElt.id = splitted[1];
+      fieldsetElt.innerHTML = "<legend>" + splitted[1].toUpperCase() + "</legend>";
     }
     else{
       //Récupération du div s'il existe
-      var divElt = document.getElementById(this.div);
+      var fieldsetElt = document.getElementById(splitted[1]);
     }
-    //Ajout des champs au div puis affichage
-    divElt.innerHTML += "<label for=\"" + this.name + "\">" + this.label + "</label>";
-    divElt.innerHTML += "<input type=\"text\" name=\"" + this.name + "\" id=\"" + this.name + "\" required/><br>";
-    document.getElementById("gestion").appendChild(divElt);
+    return fieldsetElt;
   },
 
   //Vérifcation que le champ contient un nombre et seulement un nombre
